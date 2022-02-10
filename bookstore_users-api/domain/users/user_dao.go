153d308815ce1671437ac7a9ae3f705efa17021c
Caner Kaya <caner.kaya@bilyoner.com>
2022-02-10 14:58:08 +0300

    mysql config added

--- bookstore_users-api/domain/users/user_dao.go
+++ bookstore_users-api/domain/users/user_dao.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"bookstore_users-api/utils/date_utils"
 	"bookstore_users-api/utils/errors"
 	"fmt"
 )
@@ -31,6 +32,7 @@ func (user *User) Save() *errors.RestErr {
 		}
 		return errors.NewBadRequestError(fmt.Sprintf("User %d already exists", user.Id))
 	}
+	user.DateCreated = date_utils.GetNowString()
 	usersDB[user.Id] = user
 	return nil
 }
