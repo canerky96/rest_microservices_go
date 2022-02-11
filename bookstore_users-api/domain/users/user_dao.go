ed21c3cb4be0835f353a32b52417539ae000ed7e
Caner Kaya <caner.kaya@bilyoner.com>
2022-02-11 16:05:21 +0300

    mysql configuration added.

--- bookstore_users-api/domain/users/user_dao.go
+++ bookstore_users-api/domain/users/user_dao.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"bookstore_users-api/datasources/mysql/users_db"
 	"bookstore_users-api/utils/date_utils"
 	"bookstore_users-api/utils/errors"
 	"fmt"
@@ -11,6 +12,10 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
+
 	result := usersDB[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("User %d not found", user.Id))
